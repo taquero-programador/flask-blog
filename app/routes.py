3f67a85246260458e0293d0435657c027be78876
taquero-programador <jrangel93@protonmail.com>
2022-03-31 20:09:55 -0600

    flaks 02 - base

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello, world!'
+	user = {'username': 'Javier'}
+	posts = [
+		{
+			'author': {'username': 'Jenn'},
+			'body': 'Maestra de ingles.'
+		},
+		{
+			'author': {'username': 'Diana'},
+			'body': 'Encargada de moldes.'
+
+		},
+		{
+			'author': {'username': 'Ivan'},
+			'body': 'El peñita.'
+
+		}
+	]
+	return render_template('index.html',title='Home', user=user, posts=posts)
