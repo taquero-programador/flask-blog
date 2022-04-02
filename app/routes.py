d715d25520b9d8c941f41d4f4e2cebec97e486e5
taquero-programador <jrangel93@protonmail.com>
2022-04-01 20:17:48 -0600

    flaks 04 - models

--- app/routes.py
+++ app/routes.py
@@ -29,6 +29,7 @@ def index():
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('login request for user {}, remember_me=()',format(form.username.data, form.remember_me.data))
+		flash('login request for user {}, remember_me=()'.format(
+			form.username.data, form.remember_me.data))
 		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign In', form=form)
