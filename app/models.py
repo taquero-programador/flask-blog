a96ca1797534401208f789c291b43faed469a55a
taquero-programador <jrangel93@protonmail.com>
2022-04-03 04:40:23 -0500

    flask 05 - login user

--- app/models.py
+++ app/models.py
@@ -1,26 +1,26 @@
-from datetime import datetime
 from app import db, login
-from flask_login import UserMixin
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
-	password_hash = db.Column(db.String(128))
+	password_hash = db.Column(db.String(164))
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 
 
-	def set_password(self, password):
-		self.password_hash = generate_password_hash(password)
-
-
-	def check_password():
-		return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
@@ -34,6 +34,6 @@ class Post(db.Model):
 		return '<Post {}>'.format(self.body)
 
 
-@login.user.loader
-def load_user():
+@login.user_loader
+def load_user(id):
 	return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,15 @@
-from flask import render_template, flash, redirect
-from app import app
-from app.forms import LoginForm
-from flask_login import currents_user, login_user, logout_user
+from flask import render_template, flash, redirect, request, url_for
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-	user = {'username': 'Javier'}
+	# user = {'username': 'Javier'}
 	posts = [
 		{
 			'author': {'username': 'Jenn'},
@@ -24,12 +26,12 @@ def index():
 
 		}
 	]
-	return render_template('index.html',title='Home', user=user, posts=posts)
+	return render_template('index.html',title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if currents_user.is_authenticated:
+	if current_user.is_authenticated:
 		return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
@@ -38,12 +40,30 @@ def login():
 			flash('Invalid username or password')
 			return redirect(url_for('login'))
 		login_user(user, remember=form.remember_me.data)
-		return redirect(url_for('index'))
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('index')
+		return redirect(next_page)
 	return render_template('login.html', title='Sign In', form=form)
+
 
 
 @app.route('/logout')
 def logout():
-	login_user()
+	logout_user()
 	return redirect(url_for('index'))
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
